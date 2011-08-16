295d3eb0970c1949cb9886953a7d67a6babe8b4f
Paul Ingles <paul@oobaloo.co.uk>
2011-08-16 18:05:44 +0100

    memoize next-value calculation

--- 1.6.1/clj/3n.clj
+++ 1.6.1/clj/3n.clj
@@ -1,16 +1,25 @@
 (ns programming-challenges.three-n)
 
-(defn- next-value
+(defn- slow-next-value
   [n]
   (if (even? n)
     (/ n 2)
     (+ (* n 3) 1)))
 
+(def next-value (memoize slow-next-value))
+
 (defn- collatz-seq
   ([n]
      (collatz-seq n [n]))
   ([n xs]
-     (if (= n 1)
-       xs
-       (let [val (next-value n)]
-         (collatz-seq val (conj xs val))))))
+     (lazy-seq
+      (if (= n 1)
+        xs
+        (let [val (next-value n)]
+          (collatz-seq val (conj xs val)))))))
+
+(defn max-cycle-length
+  [i j]
+  (apply max
+         (map #(count (collatz-seq %))
+              (range i j))))
